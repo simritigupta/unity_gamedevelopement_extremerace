d8491ad3e126a3471a5b71edc95b48db4b1250be
Simriti Gupta <39794413+simritigupta@users.noreply.github.com>
2020-08-09 01:33:43 +0530

    Add files via upload

---
+++ destroyer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+                if (col.gameObject.tag == "enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
